8e434d728470623711b4ce55961e202ffe535a2d
AlphaOwl <mrmundychannel@gmail.com>
2024-05-26 03:28:44 -0700

    Added collisions with boss and temp boss file

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-	public int maxHealth = 100;
+	public int maxHealth = 3;
 	public int currentHealth;
 
 	public HealthBar healthBar;
-
-    // Start is called before the first frame update
+	
+	// Start is called before the first frame update
     void Start()
     {
 		currentHealth = maxHealth;
@@ -20,16 +20,15 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-		if (Input.GetKeyDown(KeyCode.Space))
-		{
-			TakeDamage(1);
-		}
     }
 
-	void TakeDamage(int damage)
+	public void TakeDamage(int damage)
 	{
 		currentHealth -= damage;
-
 		healthBar.SetHealth(currentHealth);
+		if (currentHealth <= 0)
+		{
+			Destroy(gameObject);
+		}
 	}
 }
