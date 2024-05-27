516722c7640743d378045dc1a727b787073ff64e
Charlize <extrabutterychar@gmail.com>
2024-05-27 11:50:44 -0700

    Set Temporary Game Over Screen to When Banana Dead

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,8 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Enemy : MonoBehaviour
 {
+    public GameManagerScript gameManager;
+
     public int health = 100;
     
     public GameObject deathEffect;
@@ -28,5 +31,7 @@ public class Enemy : MonoBehaviour
         //Uncomment later when have deatheeffects
         //Instantiate(deathEffect, transform.position, Quaternion.identity);
         Destroy(gameObject);
+        gameManager.gameOver(); // to load game over screen - will remove when boss is implemented
+
     }
 }
