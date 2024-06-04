585d338a1e662e23ae586cf419e9fd5e96b1ce90
AlphaOwl <mrmundychannel@gmail.com>
2024-06-03 22:47:13 -0700

    UI is now scaled for all sizes, edits to boss hp

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -54,7 +54,7 @@ public class Enemy : MonoBehaviour
 
     private void Update()
     {
-        if (health <= 4000 && boss) {
+        if (health <= 2500 && boss) {
            // Debug.Log("HEALTH IS LESS THAN 250");
             anim.SetTrigger("stageTwo");
         }
