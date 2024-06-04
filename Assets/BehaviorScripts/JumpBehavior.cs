4fc5d1377509bb2a51ebcf96c0d3f4db49674b5f
EPaz0 <enrique_pazg@yahoo.com>
2024-06-03 20:07:19 -0700

    Jumping changed so he doesn't walk towards player and when he jumps he moves towards player

--- Assets/BehaviorScripts/JumpBehavior.cs
+++ Assets/BehaviorScripts/JumpBehavior.cs
@@ -41,25 +41,20 @@ public class JumpBehavior : StateMachineBehaviour
             Debug.Log("Timer: " + timer);
         }
 
-
-        Vector2 target = new Vector2(playerPos.position.x, rb.position.y);
-        Vector2 newPosition = Vector2.MoveTowards(rb.position, target, speed * Time.deltaTime);
-        rb.MovePosition(newPosition);
-        Debug.Log("Moving towards player. Current position: " + rb.position);
-
         // Handle jumping
         if (jumpTimer <= 0 && !isJumping)
         {
-            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            Vector2 target = new Vector2(playerPos.position.x, rb.position.y);
+            Vector2 jumpDirection = (target - rb.position).normalized * speed;
+            rb.AddForce(new Vector2(jumpDirection.x, jumpForce), ForceMode2D.Impulse);
             isJumping = true;
             jumpTimer = jumpInterval; // Reset jump timer
-            Debug.Log("Jumping with force: " + jumpForce);
+            Debug.Log("Jumping towards player with force: " + jumpForce + " and direction: " + jumpDirection);
         }
         else
         {
             jumpTimer -= Time.deltaTime;
         }
-
 
         // Check if the boss has landed
         if (isJumping && rb.velocity.y <= 0 && Mathf.Approximately(rb.velocity.y, 0))
